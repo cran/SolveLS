48b684965d66b209ea2dfb4a55d1234d6473ea73
Kisung You <kyou@nd.edu>
2018-02-12 09:32:53 +0000

    version 1.0

---
+++ R/init.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("\n * SolveLS is an iterative solver for linear system of equations authored by Kisung You (University of Notre Dame) under License GPL 3. Please feel free to share your comments and bugs to the maintainer. \n * Underdeteremined system is currently not supported. Solvers with regularization constraints to be updated along further developments.")
+}
